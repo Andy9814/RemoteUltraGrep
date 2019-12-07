920897d8f7c99eee6d9b0a7accf9463e1eb952dd
root_v <rutvi.modhiya@gmail.com>
2019-12-06 19:46:21 -0500

    added user input command connect [ip]

--- RemoteUltraGrep_Client/Client_Main.cpp
+++ RemoteUltraGrep_Client/Client_Main.cpp
@@ -1,14 +1,54 @@
 #include<iostream>
+#include<sstream>
+#include<string>
 #include "Socket.hpp"
-int main() {
+
+using namespace std;
+
+int main(int argc, char* argv) {
+
+	string ip = "";
+	string command = "";
+
+	if (argc == 1)
+	{
+		ip = "127.0.0.1";
+	}
+	else
+	{
+		ip = argv[1];
+	}
+
+	cout << "Please connect to the server (connect [ip]): ";
+	//cin >> command;
+	getline(cin, command);
+	cout << endl;
+
+	//cout << command << endl;
+
+	istringstream iss(command);
+	string word = "";
+	string connectIP = "";
+
+	while (iss >> word)
+	{
+		if (word != "connect" && word != "")
+		{
+			connectIP = word;
+		}
+		else
+			connectIP = "127.0.0.1";
+	}
+
+	cout << connectIP << endl;
 
 	//std::shared_ptr<Socket> s;
-	Socket s;
-	s.initializeWinSock();
-	s.createSocket();
-	s.connectSocket();
+	//Socket s;
+	//.initializeWinSock();
+	//s.createSocket();
+	//s.connectSocket();
 	//s.sendBytes("Hello");
 	//s.receiveBytes();
-	s.client_sendRecv();
+	//s.client_sendRecv();
 
-}
+} 
